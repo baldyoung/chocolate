10d8a16ae0b47910f40b613e94baeb654cda92a5
NoTeaNoLife <791178881@qq.com>
2021-03-12 17:42:41 +0800

    kk

--- server/src/main/java/com/xh/chocolate/common/daoModule/StudentClassDao.java
+++ server/src/main/java/com/xh/chocolate/common/daoModule/StudentClassDao.java
@@ -3,6 +3,7 @@ package com.xh.chocolate.common.daoModule;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,9 +14,13 @@ public interface StudentClassDao {
 
     void insertStudentClass(Map param);
 
+    void insertStudentClassList(@Param("studentClassList")List<Map> studentClassList);
+
     void deleteStudentClass(@Param("id")Integer id);
 
     void updateStudentClass(Map param);
 
     Map selectStudentClass(@Param("id")Integer id);
+
+    List<Map> queryAllStudentClass();
 }
--- server/src/test/java/com/xh/chocolate/ChocolateApplicationTests.java
+++ server/src/test/java/com/xh/chocolate/ChocolateApplicationTests.java
@@ -1,19 +1,48 @@
 package com.xh.chocolate;
 
 import com.xh.chocolate.common.daoModule.CourseInfoDao;
+import com.xh.chocolate.common.daoModule.StudentClassDao;
 import com.xh.chocolate.common.daoModule.SubjectDao;
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class ChocolateApplicationTests {
     @Autowired
     private SubjectDao subjectDao;
+    @Autowired
+    private StudentClassDao studentClassDao;
+
+
     @Test
     void contextLoads() {
 
         subjectDao.querySubjectList();
+    }
+
+    @Test
+    void studentClassTest() {
+        Map<String, String> map = new HashMap();
+        map.put("specialtyId", "1");
+        map.put("classNumber", "20200301");
+        map.put("className", "APP软件开发01班");
+        map.put("holderStaffId", "100101");
+        map.put("classBirthday", "2020-03-06");
+        map.put("initStudentAmount", "41");
+        map.put("currentStudentAmount", "39");
+        map.put("currentStatus", "1");
+        //studentClassDao.insertStudentClass(map);
+        List<Map> list = new LinkedList();
+        list.add(map);
+        studentClassDao.insertStudentClassList(list);
+
     }
 
 }
