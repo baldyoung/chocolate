2227c8eac79bec863b9d1cde6ea837e8ca59c99c
NoTeaNoLife <791178881@qq.com>
2021-04-14 17:35:09 +0800

    kkk

--- server/src/main/resources/static/university/js/professors.js
+++ server/src/main/resources/static/university/js/professors.js
@@ -234,3 +234,40 @@ function getSubjectListForStaff() {
 	});
 	return list;
 }
+
+// ---------------------------------- 
+var subjectListBuffer = [];
+// 获取所有的学科信息
+function requestAndLoadSubjectList() {
+	$.ajax({
+		url: XConfig.serverAddress + "subject",
+		type: 'GET',
+		cache: false,
+		dataType: 'json',
+		async: true, //设置同步
+		contentType: "application/json; charset=utf-8",
+		data: null,
+		// JSON.stringify(list)
+		success: function(data) {
+			if (data.code == 0) {
+				var targetData = data.data;
+				var target = $("#subjectSelectArea");
+				target.html('<option value="-1">-- 请选择 --</option>');
+				for(var i=0; i<targetData.length; i++) {
+					var cell = targetData[i];
+					var html = '<option value="'+i+'">'+cell.subjectName+'</option>';
+					target.append(html);
+					cell.subjectId = cell.id;
+				}
+				subjectListBuffer = targetData;
+			} else {
+				//swal('获取数据失败', data.desc, 'error');
+				alert('操作失败\n'+data.desc);
+			}
+		},
+		error: function() {
+			//swal('服务器连接失败', '请检查网络是否通畅', 'warning');
+			alert('服务器连接失败');
+		}
+	});
+}
