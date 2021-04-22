cb6d91b5c5b010003fc4de0e0f2912ef561f9ace
baldyoung <791178881@qq.com>
2021-04-22 19:14:55 +0800

    kkkk

--- server/src/main/resources/static/university/js/classRoom.js
+++ server/src/main/resources/static/university/js/classRoom.js
@@ -19,6 +19,8 @@ function getAndLoadClassRoomList() {
 		success: function(data) {
 			if (data.code == 0) {
 				var targetData = data.data;
+				targetData = sortById(targetData);
+				$('#dataLengthArea').text(targetData.length + '条数据');
 				var target = $('#classRoomListArea');
 				target.html('');
 				$.each(targetData, function(index, cell) {
@@ -27,7 +29,7 @@ function getAndLoadClassRoomList() {
 					cell.typeName = (cell.typeFlag == '1' ? '电脑间' : '非电脑间');
 					cell.classRoomSimpleInfo = cell.classRoomInfo.substring(0, 20);
 					var html = classRoomShowCellTemplate.replace('#{id}', cell.id);
-					html = html.replace('#{index}', index).replace('#{index}', index);
+					html = html.replace('#{index}', index).replace('#{index}', index).replace('#{index}', index);
 					html = html.replace('#{classRoomName}', cell.classRoomName);
 					html = html.replace('#{classRoomNumber}', cell.classRoomNumber);
 					html = html.replace('#{typeName}', cell.typeName);
@@ -67,9 +69,9 @@ function getAndLoadClassRoom(index) {
 function getEditPanelInfo() {
 	var cell = {
 		id : currentClassRoomId,
-		className : $('#newClassRoomName').val(),
+		classRoomName : $('#newClassRoomName').val(),
 		classRoomNumber : $('#newClassRoomNumber').val(),
-		typeflag : $('#newClassRoomType').val(),
+		typeFlag : $('#newClassRoomType').val(),
 		classRoomInfo : $('#newClassRoomInfo').val(), 
 		standardPeopleAmount : $('#newClassRoomNameStandardPeopleAmount').val() 
 	};
@@ -78,17 +80,95 @@ function getEditPanelInfo() {
 
 function addOrUpdateClassRoom() {
 	var newClassRoom = getEditPanelInfo();
-	if (isEmpty(newClassRoom.className, newClassRoom.typeFlag, newClassRoom.standardPeopleAmount)) {
+	if (isEmpty(newClassRoom.classRoomName, newClassRoom.typeFlag, newClassRoom.standardPeopleAmount)) {
 		alert('请补全信息');
+		console.log(newClassRoom);
 		return ;
 	}
-	
+	if (undefined != newClassRoom.id) {
+		updateClassRoomInfo(newClassRoom);
+		return;
+	}
+	var list = [newClassRoom];
+	$.ajax({
+		url: XConfig.serverAddress + "studentClassRoom",
+		type: 'POST',
+		cache: false,
+		dataType: 'json',
+		async: true, //设置同步
+		contentType: "application/json; charset=utf-8",
+		data: JSON.stringify(list),
+		success: function(data) {
+			if (data.code == 0) {
+				var targetData = data.data;
+				resetEditPanel();
+				alert('新增成功');
+			} else {
+				//swal('获取数据失败', data.desc, 'error');
+				alert('操作失败\n'+data.desc);
+			}
+		},
+		error: function() {
+			//swal('服务器连接失败', '请检查网络是否通畅', 'warning');
+			alert('服务器连接失败');
+		}
+	});
 }
 
 function updateClassRoomInfo(classRoom) {
-	
+	$.ajax({
+		url: XConfig.serverAddress + "studentClassRoom/" + classRoom.id,
+		type: 'PATCH',
+		cache: false,
+		dataType: 'json',
+		async: true, //设置同步
+		contentType: "application/json; charset=utf-8",
+		data: JSON.stringify(classRoom),
+		success: function(data) {
+			if (data.code == 0) {
+				var targetData = data.data;
+				alert('修改成功');
+			} else {
+				//swal('获取数据失败', data.desc, 'error');
+				alert('操作失败\n'+data.desc);
+			}
+		},
+		error: function() {
+			//swal('服务器连接失败', '请检查网络是否通畅', 'warning');
+			alert('服务器连接失败');
+		}
+	});
 }
 
+function deleteClassRoom(index) {
+	var cell = classRoomListBuffer[index];
+	if(!confirm("确定删除 " + cell.classRoomName + " 吗?")) {
+		return;
+	}
+	$.ajax({
+		url: XConfig.serverAddress + "studentClassRoom/" + cell.id,
+		type: 'DELETE',
+		cache: false,
+		dataType: 'json',
+		async: true, //设置同步
+		contentType: "application/json; charset=utf-8",
+		data: null,
+		success: function(data) {
+			if (data.code == 0) {
+				var targetData = data.data;
+				getAndLoadClassRoomList();
+				alert('删除成功');
+			} else {
+				//swal('获取数据失败', data.desc, 'error');
+				alert('操作失败\n'+data.desc);
+			}
+		},
+		error: function() {
+			//swal('服务器连接失败', '请检查网络是否通畅', 'warning');
+			alert('服务器连接失败');
+		}
+	});
+}
 
 
 
