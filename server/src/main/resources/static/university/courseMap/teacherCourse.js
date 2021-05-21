e1d70f11624c3d8b65983674b30e71162241b2d6
NoTeaNoLife <791178881@qq.com>
2021-05-21 17:58:49 +0800

    kkk

--- server/src/main/resources/static/university/courseMap/teacherCourse.js
+++ server/src/main/resources/static/university/courseMap/teacherCourse.js
@@ -7,7 +7,34 @@ var courseIndexTimes = ['', '8:30 - 9:10', '9:20 - 10:00', '10:20 - 11:00', '11:
 	'15:50 - 16:30', '?', '?', '?'
 ];
 $(function() {
-
+	console.log("start");
+	var courseInfoList = [{
+		dayTimeList : ['d1t1', 'd3t3'],
+		className : 'APP2002',
+		courseName : 'Java开发',
+		teacherName : '刘老师',
+		startDate : '1621590710534',
+		endDate : '1621590910534'
+	},{
+		dayTimeList : ['d3t1', 'd6t3'],
+		className : 'APP2002',
+		courseName : 'JavaScript开发',
+		teacherName : '刘老师',
+		startDate : '1621590710534',
+		endDate : '1621590910534'
+	},{
+		dayTimeList : ['d3t7', 'd6t7'],
+		className : 'APP2002',
+		courseName : '数据库开发',
+		teacherName : '刘老师',
+		startDate : '1621590710534',
+		endDate : '1621590910534'
+	}];
+	
+	$.each(courseInfoList, function(i, cell) {
+		courseMap.setCourse(cell);
+	});
+	
 	return;
 	var test = $('#test3');
 	var temp =
@@ -56,10 +83,21 @@ var courseMap = {
 			courseInfo = {
 				dayTimeList : ['d1t1'],
 				courseName : '',
-				teacherName : ''
+				teacherName : '',
+				startDate : '时间戳',
+				endDate : '时间戳'
 			}
 		*/
-		
+		console.log(courseInfo);
+		$.each(courseInfo.dayTimeList, function(index, cell){
+			var t = $('#'+cell);
+			var temp = '<div style="max-height:100%; height:50px; overflow:visible !important; "><div style="height:100%; background:green; border-radius:5px;">';
+			temp += '<div style="height:33%; text-align:center;">语文</div>';
+			temp += '<div style="height:33%; text-align:center;">王龙庆</div>';
+			temp += '<div style="height:33%; text-align:center;">教学楼303</div>';
+			temp += '</div></div>';
+			t.append(temp);
+		});
 	}
 	
 };
